8f09b7be78689111eb8e08e38dd3eb1304aa0ad7
Bharti Kuldeep <bhartikuldeep2001@rediffmail.com>
2026-04-09 11:08:46 +0530

    Create app.php

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'app_name' => 'EduNexus',
+    'database' => [
+        'driver' => 'sqlite',
+        'database' => __DIR__ . '/../database/database.sqlite',
+    ],
+];
